bbe7b3a6ec07f40346863e35a819ca9e97013fdf
yuuumbk <hikoki.y@gmail.com>
2022-07-24 19:17:32 +0900

    npm install vite-react-jsx

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const App = () => {
     return (
         <h1>Hello World!</h1>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import reactJsx from 'vite-react-jsx';
 
 export default defineConfig({
     plugins: [
@@ -10,5 +11,6 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        reactJsx(),
     ],
 });
